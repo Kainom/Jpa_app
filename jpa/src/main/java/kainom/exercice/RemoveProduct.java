b847131137fd3c471142211d3d44a2b32f6e0fe8
Kainom <eleuteriokaina@gmail.com>
2024-09-02 11:35:04 -0300

    The same sequence,now is the remove class

--- jpa/src/main/java/kainom/exercice/RemoveProduct.java
+++ jpa/src/main/java/kainom/exercice/RemoveProduct.java
@@ -5,21 +5,19 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 
-public class ChangeUser2 {
+public class RemoveProduct {
 
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpa");
         EntityManager em = emf.createEntityManager();
-        em.getTransaction().begin();
-
-        Product produto = em.find(Product.class,7L);
-        produto.setName("Vidas secas");
-        produto.setPrice(15000.200);
-//        em.merge(produto);
+        Product product =  em.find(Product.class,8);
+        if(product != null){
+            em.getTransaction().begin();
+            em.remove(product);
         em.getTransaction().commit();
-        em.close();
+        }
         emf.close();
 
-
     }
+
 }
